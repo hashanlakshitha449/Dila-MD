20859cf682db6a54082142baa1c50ad0438d2e4e
hashanlakshitha449 <167279853+hashanlakshitha449@users.noreply.github.com>
2024-09-03 14:04:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "ELUniYaC#Zy9rDx0H8cmwKMTeDgHSPM--m5wbD8hwE0cegBsVUfQ",
+MONGODB: process.env.MONGODB || "mongodb://mongo:PklzcEZxqrKzXyVpfynyxRkBXGdHIqdR@junction.proxy.rlwy.net:43250",
 };
